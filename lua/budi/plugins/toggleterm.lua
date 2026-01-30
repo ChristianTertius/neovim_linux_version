7bdbb0ecd7d0bd40dcc41bb6da7a5cab46db778f
ChristianTertius <ct160103@gmail.com>
2026-01-30 12:44:36 +0700

    toggle term vertical

--- lua/budi/plugins/toggleterm.lua
+++ lua/budi/plugins/toggleterm.lua
@@ -3,59 +3,69 @@ return {
 	version = "*",
 	config = function()
 		require("toggleterm").setup({
-			size = 15, -- Sedikit lebih besar untuk output yang lebih jelas
+			size = function(term)
+				if term.direction == "horizontal" then
+					return 15
+				elseif term.direction == "vertical" then
+					return vim.o.columns * 0.4
+				end
+			end,
 			open_mapping = [[<c-\>]],
 			shade_filetypes = {},
-			shade_terminals = false, -- Disable shading untuk performa
+			shade_terminals = false,
 			shading_factor = 1,
 			start_in_insert = true,
 			insert_mappings = true,
 			terminal_mappings = true,
 			persist_size = true,
-			persist_mode = true, -- Persist terminal mode
+			persist_mode = true,
 			direction = "horizontal",
-			close_on_exit = false, -- Jangan auto close agar bisa lihat output
+			close_on_exit = false,
 			shell = "zsh",
-			-- Tambahkan environment variables untuk optimasi
 			env = {
 				TERM = "xterm-256color",
-				-- Untuk Go, set GOCACHE jika belum ada
 				GOCACHE = vim.fn.expand("~/.cache/go-build"),
 			},
 			float_opts = {
 				border = "curved",
-				winblend = 0, -- Disable transparency untuk performa
+				winblend = 0,
 				width = math.floor(vim.o.columns * 0.8),
 				height = math.floor(vim.o.lines * 0.8),
 			},
 			winbar = {
 				enabled = false,
 			},
-			-- Tambahkan auto scroll untuk output panjang
 			auto_scroll = true,
 		})
 
-		-- Fungsi khusus untuk Go development
 		local Terminal = require("toggleterm.terminal").Terminal
 
-		-- Terminal khusus untuk Go
+		-- Terminal horizontal untuk Go
 		local go_terminal = Terminal:new({
 			cmd = "pwsh",
 			direction = "horizontal",
 			size = 15,
 			close_on_exit = false,
-			-- Optimasi untuk Go
 			env = {
 				GOCACHE = vim.fn.expand("~/.cache/go-build"),
 				GOPATH = vim.fn.expand("~/go"),
 			},
 		})
 
-		-- Fungsi untuk run Go dengan optimasi
 		function _G.go_run()
 			go_terminal:toggle()
-			-- Kirim command dengan optimasi
 			go_terminal:send("go run -ldflags='-s -w' main.go")
+		end
+
+		-- Terminal vertical di kanan
+		local vertical_terminal = Terminal:new({
+			direction = "vertical",
+			size = vim.o.columns * 0.4,
+			close_on_exit = false,
+		})
+
+		function _G.toggle_vertical_terminal()
+			vertical_terminal:toggle()
 		end
 
 		-- Terminal floating untuk quick commands
@@ -92,10 +102,11 @@ return {
 			lazygit:toggle()
 		end
 
-		-- Key mappings untuk optimasi workflow
+		-- Key mappings
 		vim.keymap.set("n", "<leader>gr", "<cmd>lua go_run()<CR>", { desc = "Go Run Optimized" })
 		vim.keymap.set("n", "<leader>tt", "<cmd>lua toggle_float_terminal()<CR>", { desc = "Float Terminal" })
 		vim.keymap.set("n", "<leader>tf", "<cmd>ToggleTerm<CR>", { desc = "Toggle Terminal" })
+		vim.keymap.set("n", "<leader>tv", "<cmd>lua toggle_vertical_terminal()<CR>", { desc = "Vertical Terminal" })
 		vim.keymap.set("n", "<leader>lg", "<cmd>lua toggle_lazygit()<CR>", { desc = "Lazygit (Float)" })
 	end,
 }
