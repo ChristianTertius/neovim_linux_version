aeec5368660b7819858ec06888558af87975bb55
ChristianTertius <ct160103@gmail.com>
2026-06-05 11:08:47 +0800

    chore: kanagawa transparent

--- lua/budi/plugins/colorscheme.lua
+++ lua/budi/plugins/colorscheme.lua
@@ -48,6 +48,18 @@ return {
 		"rebelot/kanagawa.nvim",
 		name = "kanagawa",
 		config = function()
+			require("kanagawa").setup({
+				transparent = true, -- background transparent
+				colors = {
+					theme = {
+						all = {
+							ui = {
+								bg_gutter = "none", -- gutter juga transparent
+							},
+						},
+					},
+				},
+			})
 			vim.cmd("colorscheme kanagawa")
 		end,
 	},
